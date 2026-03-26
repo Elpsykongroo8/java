e89ad1b401f82e3a83184ed18a7910a3abd8f406
Elpsykongroo8 <dj20250611@dge.go.kr>
2026-03-26 15:04:19 +0900

    Add files via upload

---
+++ S409.java
@@ -0,0 +1,14 @@
+package s0408;
+
+public class S409 {
+	public static void main(String[]args) {
+		int a = 0;
+		while (a < 10) {
+		    a++;
+		    if (a % 2 == 0) {
+		        continue;
+		    }
+		    System.out.println(a);
+		}
+}
+}
--- Sample.java
+++ Sample.java
@@ -1,37 +1,13 @@
-package jump2java;
+package s0408;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Sample {
     public static void main(String[] args) {
-        int month = 8;
-        String monthString = "";
-        switch (month) {
-            case 1:  monthString = "1월";
-                     break;
-            case 2:  monthString = "2월";
-                     break;
-            case 3:  monthString = "3월";
-                     break;
-            case 4:  monthString = "4월";
-                     break;
-            case 5:  monthString = "5월";
-                     break;
-            case 6:  monthString = "6월";
-                     break;
-            case 7:  monthString = "7월";
-                     break;
-            case 8:  monthString = "8월";
-                     break;
-            case 9:  monthString = "9월";
-                     break;
-            case 10: monthString = "10월";
-                     break;
-            case 11: monthString = "11월";
-                     break;
-            case 12: monthString = "12월";
-                     break;
-            default: monthString = "Invalid month";
-                     break;
+        ArrayList<String> numbers = new ArrayList<>(Arrays.asList("one", "two", "three"));
+        for (String number : numbers) {
+            System.out.println(number);
         }
-        System.out.println(monthString);
     }
 }
